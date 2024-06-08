4fca7dcb0dee1ea5e1ad1e2aa16e997f86c64d5f
arajean-saez <131237350+arajean-saez@users.noreply.github.com>
2024-06-08 22:16:45 +0800

    Add files via upload

---
+++ contracts/Assessment.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+    event Transfer(address indexed recipient, uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        balance += _amount;
+        assert(balance == _previousBalance + _amount);
+
+        emit Deposit(_amount);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        balance -= _withdrawAmount;
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        emit Withdraw(_withdrawAmount);
+    }
+
+    function transfer(address _recipient, uint256 _amount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_recipient != address(0), "Invalid recipient address");
+
+        uint _previousBalance = balance;
+        require(balance >= _amount, "Insufficient balance");
+
+        balance -= _amount;
+        assert(balance == (_previousBalance - _amount));
+
+        emit Transfer(_recipient, _amount);
+    }
+}
